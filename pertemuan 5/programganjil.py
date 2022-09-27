a257cd7db15887a31dccd9dd82f16cc18547d537
ade-m <66313509+ade-m@users.noreply.github.com>
2022-09-27 20:23:40 +0700

    berlian

--- pertemuan 5/programganjil.py
+++ pertemuan 5/programganjil.py
@@ -2,13 +2,36 @@
 import genericpath
 
 
-n=30
+n=3
 i=0
 
-#program
+#program perulangan for
 for i in range(n,0,-1):
     if i % 2 != 0 :
         print(i, ": Bilangan ganjil")
     else :
         print(i,": Bilangan Genap")
-   
+
+#program perulangan while
+i= 30
+while i>0 :
+    if i % 2 != 0 :
+        print(i, ": Bilangan ganjil")
+    else :
+        print(i,": Bilangan Genap")
+    i-=1
+
+# n =6
+
+#
+##
+###
+####
+#####
+######
+
+# n=3
+
+#
+##
+###
