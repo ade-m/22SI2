93280edf5123855d831617a18943df6c55ea090d
ade-m <66313509+ade-m@users.noreply.github.com>
2022-09-27 18:50:58 +0700

    ganjil genap

---
+++ pertemuan 5/programganjil.py
@@ -0,0 +1,14 @@
+#deklarasi
+import genericpath
+
+
+n=30
+i=0
+
+#program
+for i in range(n,0,-1):
+    if i % 2 != 0 :
+        print(i, ": Bilangan ganjil")
+    else :
+        print(i,": Bilangan Genap")
+   
--- pertemuan 5/programperulangan1.py
+++ pertemuan 5/programperulangan1.py
@@ -4,4 +4,15 @@ number = 0
 for number in range(10) :
     print("ini adalah iterasi ke ", str(number))
 
+
 print("ini adalah akhir perulangan")
+
+#deklarasi
+i = 0
+
+for i in range(9,-1,-1) :
+    print("ini adalah iterasi ke ", str(i))
+
+print("ini adalah akhir perulangan")
+
+#for (int i =10; i>=0;i--)
