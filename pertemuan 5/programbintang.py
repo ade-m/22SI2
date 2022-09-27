44409517459453f23d121c7a86bf75e32c06d2f8
ade-m <66313509+ade-m@users.noreply.github.com>
2022-09-27 20:30:34 +0700

    10 gelombang

--- pertemuan 5/programbintang.py
+++ pertemuan 5/programbintang.py
@@ -2,23 +2,23 @@
 n=int(input("Masukkan N: "))
 #mode = int(input("Masukkan Mode (1. bintang, 2.Angka): "))
 
-i=1
+x=1
+for x in range(1,n+1) :
+    i=1
+    for i in range(1,n+1) :
+        j=1
+    
+        for j in range(1,i+1) :
+            print(j,end="")
+        print("")
 
-for i in range(1,n+1) :
-    j=1
-   
-    for j in range(1,i+1) :
-        print(j,end="")
-    print("")
-
-i=1
-
-for i in range(n,0,-1) :
-    j=1
-   
-    for j in range(1,i+1) :
-        print(j,end="")
-    print("")
+    i=1
+    for i in range(n-1,0,-1) :
+        j=1
+    
+        for j in range(1,i+1) :
+            print(j,end="")
+        print("")
    # for j in range(1,i+1) :
  #   if(mode==1):
   #      print("*",end="")
